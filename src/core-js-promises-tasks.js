3b3918399ff8a018816f172f2204534d0c0316f5
t_frrolova <tatianaaleks.frolova@gmail.com>
2024-04-04 17:46:31 +0200

    feat: except for the last problem solved

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -61,8 +61,14 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return Promise.any(promises)
+    .then((res) => {
+      return Promise.resolve(res);
+    })
+    .catch((reason) => {
+      return Promise.reject(reason);
+    });
 }
 
 /**
@@ -84,8 +90,8 @@ function getFirstResolvedPromiseResult(/* promises */) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return Promise.race(promises).then((res) => res);
 }
 
 /**
@@ -99,8 +105,10 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return Promise.all(promises)
+    .then((res) => res)
+    .catch((reason) => Promise.reject(reason));
 }
 
 /**
@@ -115,8 +123,12 @@ function getAllOrNothing(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  return Promise.allSettled(promises).then((results) => {
+    return results.map((res) =>
+      res.status === 'fulfilled' ? res.value : null
+    );
+  });
 }
 
 /**
